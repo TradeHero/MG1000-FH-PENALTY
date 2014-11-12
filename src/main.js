43d83fface731af8ac60496ccd0a9ed4547184da
Li Hao <lihao6485@gmail.com>
2014-11-13 00:26:31 +0800

    #init - initial commit
    Signed-off-by: Li Hao <lihao6485@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/*
+ * TradeHero FH-Penalty Javascript Plugin v@@version
+ * http://www.tradehero.mobi/
+ * Copyright 2014, TradeHero
+ * Date: @@date
+ *
+ * Copyright (C) 2012 - 2014 by TradeHero
+ */
+
+var FHPenalty = {};
